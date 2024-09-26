415eb9bd89d17427a7c82d92012b72cf89637412
loongbatian <galh3013@gmail.com>
2024-09-26 16:21:48 +0500

    fix:本地配置setting；数据库绑定问题

--- apps/base/models.py
+++ apps/base/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-class Info(models.Model):
+class UserInfo(models.Model):
     name = models.CharField(verbose_name='测试', max_length=32)
+    password = models.CharField(verbose_name='密码', max_length=62)
--- mtb/settings.py
+++ mtb/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 '''引入本地配置'''
-try:
-    from ..local_settings import *
-except ImportError:
-    pass
+
 
 from pathlib import Path
 
@@ -79,11 +76,15 @@ WSGI_APPLICATION = 'mtb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'mtb',
+          'USER': 'root',
+          'PASSWORD': '123456',
+          'HOST': '127.0.0.1',
+          'PORT': '3306',
+      }
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
